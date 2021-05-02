d43b69ade87838d26e8182dbeeb6a84d81dd1df1
Belén Pinilla <m.pinilla04@ufromail.cl>
2021-05-01 22:18:17 -0400

    Initial commit

---
+++ src/main/java/main.java
@@ -0,0 +1,23 @@
+
+
+public class main {
+
+
+
+    public static void main(String[] args){
+
+        bienvenida();
+
+
+    }
+    public static void bienvenida () {
+        System.out.println("*******************************");
+        System.out.println("*                             *");
+        System.out.println("*          bienvenida         *");
+        System.out.println("*                             *");
+        System.out.println("*******************************");
+    }
+    public static void menu(){
+        System.out.println("¿eres nueva?");
+    }
+}
