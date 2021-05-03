3a29e4b52ddc9b0683843c22cbc6daa6c19d4c1f
Belén Pinilla <m.pinilla04@ufromail.cl>
2021-05-03 02:50:28 -0400

    modifiqué unas cosas que no me gustaron

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class main {
 
-
+    public static Scanner teclado = new Scanner(System.in);
 
     public static void main(String[] args){
 
@@ -26,29 +26,44 @@ public class main {
     }
 
     public static void menuaccion(){
-        int opcion= -1;
+
+        int opcion= teclado.nextInt();
         switch (opcion){
             case 1:
-
-                System.out.println("ingresa tu último periodo");
-                System.out.println("ingresa un promedio de tu ciclo");
+                ingresarpromedio();
+                ingresarultimafecha();
                 //debería ir una interfaz que permita ingresar una fecha y un número
-
                 break;
             case 2:
-
                 System.out.println("bienvenida de nuevo a bloodtide");
                 //aquí debería ir una interfaz que muestre la fecha del próximo periodo
                 // y la fecha de la próxima ovulación.
-
                 break;
             case 0:
                 System.out.println("adiós");
+                //se debiera cerrar la aplicación
                 break;
             default:
+                System.out.println("no es una opción válida");
+                break;
 
         }
 
+    }
+    public static void ingresarpromedio(){
+
+        System.out.println("ingresa un promedio de tu ciclo");
+        int promciclo= teclado.nextInt();
+        System.out.println("el promedio que ingresaste es "+promciclo);
+    }
+    public static void ingresarultimafecha(){
+
+        System.out.println("ingresa tu último periodo");
+        System.out.println("ingresa el día:");
+        String diaultimoperiodo= teclado.next();
+        System.out.println("ingresa el mes");
+        String mesutlimoperiodo= teclado.next();
+        System.out.println("el útlimo periodo que ingresaste es: "+diaultimoperiodo+" de "+mesutlimoperiodo);
     }
 
 
