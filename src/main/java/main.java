88b3da83d00bb4ab183dd1fed4d30d49bb0cc03a
Belén Pinilla <m.pinilla04@ufromail.cl>
2021-05-02 21:43:37 -0400

    escribí un switch case para hacer un menu un poco más interactivo

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,4 +1,4 @@
-
+import java.util.Scanner;
 
 public class main {
 
@@ -7,9 +7,10 @@ public class main {
     public static void main(String[] args){
 
         bienvenida();
-
+        menu();
 
     }
+
     public static void bienvenida () {
         System.out.println("*******************************");
         System.out.println("*                             *");
@@ -17,7 +18,38 @@ public class main {
         System.out.println("*                             *");
         System.out.println("*******************************");
     }
+
     public static void menu(){
         System.out.println("¿eres nueva?");
+        System.out.println("presiona 1 si eres nueva, presiona 2 para ver tu calendario, presiona 0 para salir");
+        menuaccion();
     }
+
+    public static void menuaccion(){
+        int opcion= -1;
+        switch (opcion){
+            case 1:
+
+                System.out.println("ingresa tu último periodo");
+                System.out.println("ingresa un promedio de tu ciclo");
+                //debería ir una interfaz que permita ingresar una fecha y un número
+
+                break;
+            case 2:
+
+                System.out.println("bienvenida de nuevo a bloodtide");
+                //aquí debería ir una interfaz que muestre la fecha del próximo periodo
+                // y la fecha de la próxima ovulación.
+
+                break;
+            case 0:
+                System.out.println("adiós");
+                break;
+            default:
+
+        }
+
+    }
+
+
 }
